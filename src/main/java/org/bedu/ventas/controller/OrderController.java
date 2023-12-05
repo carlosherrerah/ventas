5102f26a6a75f4ba7d6029b4b0cf6756304d8ae6
Dan Javier Olvera Villeda <olveradan@hotmail.com>
2023-12-05 01:35:56 -0600

    feat: OrderController findById endpoint

--- src/main/java/org/bedu/ventas/controller/OrderController.java
+++ src/main/java/org/bedu/ventas/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.bedu.ventas.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +24,8 @@ public class OrderController {
         return orderService.findAll();
     }
 
+    @GetMapping("/{orderId}")
+    public OrderDTO findById(@PathVariable long orderId) {
+        return orderService.findById(orderId);
+    }
 }
