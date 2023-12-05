8fa4646aac5f0e8c507ab3f1ba553782465dc0e7
Dan Javier Olvera Villeda <olveradan@hotmail.com>
2023-12-05 01:22:19 -0600

    refact: some refactorization in OrderController endpoint paths. I add a general @RequestMapping config for orders '/orders'. Also i made a refactorization in Order.java entity, i delete Lombok @Data annotation and replaced with @Getter and @Setter annotations

--- src/main/java/org/bedu/ventas/controller/OrderController.java
+++ src/main/java/org/bedu/ventas/controller/OrderController.java
@@ -7,17 +7,20 @@ import org.bedu.ventas.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @GetMapping("/orders")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderDTO> findAll() {
         return orderService.findAll();
     }
+
 }
