b1188dd88f2a5ddb7d183b30bff80fd7caf308c9
Dan Javier Olvera Villeda <olveradan@hotmail.com>
2023-12-06 23:24:05 -0600

    feat: OpenAPI dependency has been added with his configuration class

---
+++ src/main/java/org/bedu/ventas/config/OpenAPIConfig.java
@@ -0,0 +1,15 @@
+package org.bedu.ventas.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API Ventas",
+        description = "API para la administración de ventas",
+        version = "v0.1.0"
+    )
+)
+public class OpenAPIConfig {
+    
+}
