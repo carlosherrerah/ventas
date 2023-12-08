3e813c07fe7b0aa00764092ca1d3ee52fb81a087
Dan Javier Olvera Villeda <olveradan@hotmail.com>
2023-12-07 23:35:47 -0600

    feat: Documentation for GET endpoint in Order controller

--- src/main/java/org/bedu/ventas/controller/OrderController.java
+++ src/main/java/org/bedu/ventas/controller/OrderController.java
@@ -12,18 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Endpoints de ordenes de compra", description = "CRUD de ordenes de compra")
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     @Autowired
     private OrderService orderService;
-
+    
+    @Operation(summary = "Obtiene una lista de toda las ordenes de compra")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<OrderDTO> findAll() {
         return orderService.findAll();
     }
 
+    @Operation(summary = "Obtiene una orden de compra según su ID")
     @GetMapping("/{orderId}")
     public OrderDTO findById(@PathVariable long orderId) {
         return orderService.findById(orderId);
--- src/main/java/org/bedu/ventas/dto/OrderDTO.java
+++ src/main/java/org/bedu/ventas/dto/OrderDTO.java
@@ -4,14 +4,20 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class OrderDTO {
+    @Schema(description = "Identificador de la orden de compra", example = "2")
     private long orderid;
 
+    @Schema(
+        description = "Empleado que realizo la orden de compra"
+    )
     private EmployeeDTO employee;
 
+    @Schema(description = "Fecha de orden de compra", example = "2023-12-02")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date orderdate;
 }
