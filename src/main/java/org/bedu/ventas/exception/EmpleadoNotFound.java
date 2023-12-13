915bbed8b4ddd62f8665d7054505660c1d5d30c8
= <=>
2023-12-12 21:58:23 -0600

    borrar archivos

--- src/main/java/org/bedu/ventas/exception/EmpleadoNotFound.java
+++
@@ -1,9 +0,0 @@
-package org.bedu.ventas.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class EmpleadoNotFound  extends RuntimeException {
-     public EmpleadoNotFound(long employeeId){
-        super("Empleado No Encontrado");
-    }
-}
